72d2bb569b1ae4e8ba0ed15ab9d19761c4ab0623
vishwa-jith <vishwajith567@gmail.com>
2021-03-28 17:57:50 +0530

    update checkup report functionality

---
+++ angular/src/app/services/checkupReport/checkup-report.service.model.ts
@@ -0,0 +1,9 @@
+export interface CheckupReport {
+  id: number;
+  shop_name: string;
+  trainer_name: string;
+  report: string;
+  date: string;
+  days: number;
+  amount: number;
+}
--- angular/src/app/services/getAppointments/get-appointments.service.ts
+++ angular/src/app/services/getAppointments/get-appointments.service.ts
@@ -6,10 +6,15 @@ import { Appointment } from './get-appointments.services.model';
 })
 export class GetAppointmentsService {
   appointments: Appointment[] = [
-    { id: 1, booking_id: 1, trainer_name: 'sample', date: '12/03/2021' },
-    { id: 2, booking_id: 2, trainer_name: 'sample', date: '12/03/2021' },
-    { id: 3, booking_id: 3, trainer_name: 'sample', date: '12/03/2021' },
-    { id: 4, booking_id: 4, trainer_name: 'sample', date: '12/03/2021' },
+    { id: 1, booking_id: 1, trainer_name: 'Lokesh R', date: '22/03/2021' },
+    { id: 2, booking_id: 2, trainer_name: 'Vishwajith V', date: '05/11/2021' },
+    {
+      id: 3,
+      booking_id: 3,
+      trainer_name: 'Suresh Anand K',
+      date: '12/05/2021',
+    },
+    { id: 4, booking_id: 4, trainer_name: 'Sai Prasadth', date: '02/09/2021' },
   ];
   getAppointments = () => {
     return this.appointments;
