b659edf61fd06f4edf1c0707f5e23d9dbfd464e5
vishwa-jith <vishwajith567@gmail.com>
2021-03-29 19:03:02 +0000

    setup db

--- spring/src/main/java/com/example/controller/HomeController.java
+++ spring/src/main/java/com/example/controller/HomeController.java
@@ -1,13 +1,22 @@
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
+	
+	@Autowired
+	JdbcTemplate jdbc;
 
 	@GetMapping("/")
 	public String testApi() {
+		String sql="select * from user;";
+		
+		jdbc.qu
+		
 		return "Api is working!";
 	}
 }
