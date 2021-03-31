81e2f5df38a129c024abda9d08ea07e34e9e32c1
vishwa-jith <vishwajith567@gmail.com>
2021-03-31 10:56:44 +0000

    add user controller routes

---
+++ spring/src/main/java/com/example/Dao/User.java
@@ -0,0 +1,96 @@
+package com.example.Dao;
+
+public class User {
+    private String id;
+    private String email;
+    private String password;
+    private String username;
+    private String mobileNumber;
+    private Boolean active;
+    private String role;
+    private String shopName;
+    private int experience;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getShopName() {
+        return shopName;
+    }
+
+    public void setShopName(String shopName) {
+        this.shopName = shopName;
+    }
+
+    public int getExperience() {
+        return experience;
+    }
+
+    public void setExperience(int experience) {
+        this.experience = experience;
+    }
+
+    public User() {
+    }
+
+    @Override
+    public String toString() {
+        return "User [active=" + active + ", email=" + email + ", experience=" + experience + ", id=" + id
+                + ", mobileNumber=" + mobileNumber + ", password=" + password + ", role=" + role + ", shopName="
+                + shopName + ", username=" + username + "]";
+    }
+
+}
