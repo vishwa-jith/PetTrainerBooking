a6c42d3bd9224c5c58edd3c7f11c3c358fe6b62d
swami2001 <swamijegan@gmail.com>
2021-03-31 04:58:30 +0000

    User

---
+++ spring/src/main/java/com/example/Dao/User.java
@@ -0,0 +1,107 @@
+package com.example.Dao;
+
+public class User {
+    private int id;
+    private String email;
+    private String password;
+    private String username;
+    private String mobileNumber;
+    private Boolean active;
+    private String role;
+    private String shopName;
+    private int experience;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+  public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getShopName() {
+        return shopName;
+    }
+
+    public void setShopName(String shopName) {
+        this.shopName = shopName;
+    }
+
+    public int getExperience() {
+        return experience;
+    }
+
+    public void setExperience(int experience) {
+        this.experience = experience;
+    }
+
+    public User() {
+    }
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public User(String email, String password, String username, String mobileNumber) {
+        this.email = email;
+        this.password = password;
+        this.username = username;
+        this.mobileNumber = mobileNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "User [active=" + active + ", email=" + email + ", experience=" + experience + ", id=" + id
+                + ", mobileNumber=" + mobileNumber + ", password=" + password + ", role=" + role + ", shopName="
+                + shopName + ", username=" + username + "]";
+    }
+
+}
--- spring/src/main/java/com/example/controller/HomeController.java
+++ spring/src/main/java/com/example/controller/HomeController.java
@@ -1,13 +1,87 @@
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import com.example.Dao.User;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import org.springframework.dao.DataAccessException;
 @RestController
 public class HomeController {
 
-	@GetMapping("/")
-	public String testApi() {
-		return "Api is working!";
+	@Autowired
+	JdbcTemplate jdbc;
+
+	@GetMapping("/createUser")
+	public String welcome() {
+		String sql = "CREATE TABLE user (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, email VARCHAR(30) NOT NULL UNIQUE, password VARCHAR(30) NOT NULL, username VARCHAR(50) NOT NULL UNIQUE, mobileNumber VARCHAR(11), active BOOLEAN DEFAULT true, role VARCHAR(20) NOT NULL DEFAULT 'owner', shopName VARCHAR(20), experience INT(2))";
+		jdbc.execute(sql);
+		return "Welcome";
+	}
+
+	@PostMapping(value = "/signup")
+	public String signup(@RequestBody User user) {
+		User userEmail = jdbc.query("select * from user where email='" + user.getEmail() + "';",
+				new ResultSetExtractor<User>() {
+					@Override
+					public User extractData(ResultSet rs) throws SQLException, DataAccessException {
+						User e = new User();
+						while (rs.next()) {
+							e.setEmail(rs.getString(2));
+						}
+						return e;
+					}
+				});
+		User userUsername = jdbc.query("select * from user where username='" + user.getUsername() + "';",
+				new ResultSetExtractor<User>() {
+					@Override
+					public User extractData(ResultSet rs) throws SQLException, DataAccessException {
+						User e = new User();
+						while (rs.next()) {
+							e.setUsername(rs.getString(4));
+						}
+						return e;
+					}
+				});
+		if (userEmail.getEmail() != null) {
+			return "User already exist with this email";
+		} else if (userUsername.getUsername() != null) {
+			return "User already exist with this username";
+		} else {
+			String query = "insert into user (username, email, password, mobileNumber) values ('" + user.getUsername()
+					+ "','" + user.getEmail() + "','" + user.getPassword() + "','" + user.getMobileNumber() + "')";
+			jdbc.update(query);
+			return "Signup Successful";
+		}
+	}
+
+	@PostMapping(value = "/login")
+	public String login(@RequestBody User user) {
+		User logUser = jdbc.query("select * from user where email='" + user.getEmail() + "';",
+				new ResultSetExtractor<User>() {
+					@Override
+					public User extractData(ResultSet rs) throws SQLException, DataAccessException {
+
+						User e = new User();
+						while (rs.next()) {
+							e.setId(rs.getInt(1));
+							e.setEmail(rs.getString(2));
+							e.setPassword(rs.getString(3));
+						}
+						return e;
+					}
+				});
+		if (logUser.getEmail() == null) {
+			return "Not such account exist";
+		} else if (logUser.getEmail().equals(user.getEmail()) && logUser.getPassword().equals(user.getPassword())) {
+			return "Login Successful";
+		} else {
+			return "Wrong username or password";
+		}
 	}
 }
