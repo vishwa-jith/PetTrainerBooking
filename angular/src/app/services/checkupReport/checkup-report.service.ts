5c83516b652ae182eeba6d5cddac920fd9481618
vishwa-jith <vishwajith567@gmail.com>
2021-04-02 23:39:23 +0530

    integrate trainer report

--- angular/src/app/services/checkupReport/checkup-report.service.ts
+++ angular/src/app/services/checkupReport/checkup-report.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { CheckupReport } from './checkup-report.service.model';
 import { HttpClient } from '@angular/common/http';
 import { AuthService } from '../auth/auth.service';
 
@@ -9,52 +8,15 @@ import { AuthService } from '../auth/auth.service';
 export class CheckupReportService {
   constructor(private http: HttpClient, private authService: AuthService) {}
 
-  checkupReports: CheckupReport[] = [
-    {
-      id: 1,
-      shop_name: 'ABC Shop',
-      trainer_name: 'Lokesh R',
-      report: 'Hey this is a sample report, beware of it',
-      date: '22/03/2021',
-      days: 7,
-      amount: 3000,
-    },
-    {
-      id: 2,
-      shop_name: 'LMN Shop',
-      trainer_name: 'Vishwajith V',
-      report: 'Hey this is a sample report, beware of it',
-      date: '05/11/2021',
-      days: 7,
-      amount: 23000,
-    },
-    {
-      id: 3,
-      shop_name: 'KPY Shop',
-      trainer_name: 'Suresh Anand K',
-      report: 'Hey this is a sample report, beware of it',
-      date: '12/05/2021',
-      days: 7,
-      amount: 1250,
-    },
-    {
-      id: 4,
-      shop_name: 'LEM Shop',
-      trainer_name: 'Sai Prasadth',
-      report: 'Hey this is a sample report, beware of it',
-      date: '02/09/2021',
-      days: 7,
-      amount: 3050,
-    },
-  ];
-
   baseUrl =
     'https://8080-bafeaefeddfbbbacedbefccaeeabbfbebdcacd.examlyiopb.examly.io';
 
   userDetails = JSON.parse(this.authService.getuserDetails());
 
-  checkupReport = (reportId: number) => {
-    return this.checkupReports.filter(({ id }) => id === reportId)[0];
+  checkupReportForOwner = (trainerId: string): any => {
+    return this.http.get<any>(
+      this.baseUrl + `/checkupReport/${this.userDetails.id}/${trainerId}`
+    );
   };
 
   checkupReportForTrainer = (userId: string) => {
--- angular/src/app/user-report/user-report.component.ts
+++ angular/src/app/user-report/user-report.component.ts
@@ -1,29 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { GetAppointmentsService } from '../services/getAppointments/get-appointments.service';
 import { CheckupReportService } from '../services/checkupReport/checkup-report.service';
-import { CheckupReport } from '../services/checkupReport/checkup-report.service.model';
+import { Report } from '../services/checkupReport/checkup-report.service.model';
+
 @Component({
   selector: 'app-user-report',
   templateUrl: './user-report.component.html',
   styleUrls: ['./user-report.component.css'],
 })
 export class UserReportComponent implements OnInit {
-  trainerId: number | undefined;
-  appointments;
-  report: CheckupReport | undefined;
+  trainerId: string | undefined;
+  appointments: any;
+  report: Report | undefined;
+  selectedReport: any;
   constructor(
     private getAppointmentService: GetAppointmentsService,
     private checkupReportService: CheckupReportService
   ) {
-    this.appointments = this.getAppointmentService.getAppointments();
+    this.getAppointmentService.getAppointments().subscribe((appoint) => {
+      this.appointments = appoint;
+    });
   }
 
-  handleTrainerId(id: number): void {
+  handleTrainerId(id: string): void {
     if (this.trainerId !== id) {
       this.trainerId = id;
-      this.report = this.checkupReportService.checkupReport(this.trainerId);
+      this.report = this.checkupReportService
+        .checkupReportForOwner(this.trainerId)
+        .subscribe((report: Report) => {
+          this.selectedReport = report;
+        });
     } else {
       this.trainerId = undefined;
+      this.selectedReport = null;
     }
   }
 
